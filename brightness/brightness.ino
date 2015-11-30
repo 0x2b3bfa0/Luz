bbf6a27d74ea40f0b2eba8491574d38ab10b3cd2
Helio <crushedice2000@gmail.com>
2015-11-30 17:07:38 +0100

    Working

--- brightness/brightness.ino
+++ brightness/brightness.ino
@@ -3,8 +3,10 @@
 #include <ESP8266WebServer.h>
 #include <FS.h>
 
+
 #define PIN 16
 #define DEBUG
+
 
 IPAddress apIP(192, 168, 1, 1);
 ESP8266WebServer webServer(80);
@@ -13,38 +15,46 @@ DNSServer dnsServer;
 const char *ssid = "Lampara";
 const char *password = "Espressif";
 
-boolean isNumber(String str){
-  boolean isNum=false;
-  for(byte i=0;i<str.length();i++) {
-    isNum = isDigit(str.charAt(i));
-    if(!isNum) return false;
-  }
-  return isNum;
-}
 
-void root() {
-  int value;
+void handleRoot() {
   File file;
-  String result = webServer.arg("brightness");
+  int pwm_value;
+  String brightness = webServer.arg("brightness");
   
-  if(isNumber(result)) {
-    value = result.toInt();
-  }
+  pwm_value = brightness.toInt();
 
- if(value <= 100) {
-   value = map(value, 0, 100, 0, 1024);
-   analogWrite(PIN, value);
-   webServer.send(200, "text/plain", "[OK]");
- } else if(result == "-1") {
-   file = SPIFFS.open("/rant.html", "r");
-   webServer.streamFile(file, "text/html");
-   file.close();
- } else {
-   file = SPIFFS.open("/index.html", "r");
-   webServer.streamFile(file, "text/html");
-   file.close();
- }
+  if(pwm_value <= 100 && pwm_value > 0 || brightness == "0") {
+    pwm_value = map(pwm_value, 0, 100, 0, 1024);
+    webServer.send(200, "text/plain", "OK");
+    analogWrite(PIN, pwm_value);
+  } else if(brightness == "-1" || brightness == "'") {
+    file = SPIFFS.open("/easter_egg.html", "r");
+    webServer.streamFile(file, "text/html");
+    file.close();
+  } else {
+    file = SPIFFS.open("/index.html", "r");
+    webServer.streamFile(file, "text/html");
+    file.close();
+  }
 }
+
+// for file in `ls -A1`; do curl -F "file=@$PWD/$file" esp8266fs.local/; done
+void handleUpload() {
+  File file;
+  HTTPUpload& upload = webServer.upload();
+  if(upload.status == UPLOAD_FILE_START) {
+    String filename = upload.filename;
+    if(!filename.startsWith("/")) filename = "/" + filename;
+    file = SPIFFS.open(filename, "w");
+  } else if(upload.status == UPLOAD_FILE_WRITE) {
+    if(file)
+      file.write(upload.buf, upload.currentSize);
+  } else if(upload.status == UPLOAD_FILE_END) {
+    if(file)
+      file.close();
+  }
+}
+
 
 void setup() {
   Serial.begin(115200);
@@ -59,10 +69,12 @@ void setup() {
 
   dnsServer.start(53, "*", apIP);
 
-	webServer.on("/", root);
-  webServer.onNotFound(root);
+  webServer.on("/", handleRoot);
+  webServer.onNotFound(handleRoot);
+  webServer.onFileUpload(handleUpload);
 	webServer.begin();
  }
+
 
 void loop() {
   dnsServer.processNextRequest();
