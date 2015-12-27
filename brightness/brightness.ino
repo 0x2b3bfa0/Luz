4cd9e98dfdde3010cad84d8b58b7f401d205611c
Helio <crushedice2000@gmail.com>
2015-12-27 12:10:31 +0100

    Passwordless

--- brightness/brightness.ino
+++ brightness/brightness.ino
@@ -69,7 +69,8 @@ void setup() {
 
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-	WiFi.softAP(ssid, password);
+	WiFi.softAP(ssid); // 	WiFi.softAP(ssid, password);
+
 
   dnsServer.start(53, "*", apIP);
 
